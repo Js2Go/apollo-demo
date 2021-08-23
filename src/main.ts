cb9cda5b21c3ff259d99eef323c1e474af475a71
mazi <602281666@qq.com>
2021-08-23 11:05:40 +0800

    添加持久化查询

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import { createApp } from 'vue'
 import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { createUploadLink } from 'apollo-upload-client'
+import { sha256 } from 'crypto-hash'
 import ElementPlus from 'element-plus'
 
 import 'element-plus/lib/theme-chalk/index.css'
 
 import App from './App.vue'
 
-const httpLink: any = createUploadLink({
+const linkChain = createPersistedQueryLink({
+  sha256,
+  useGETForHashedQueries: true
+}).concat(createUploadLink({
   uri: 'http://localhost:8899/graphql',
-})
+}) as any)
 
 const cache = new InMemoryCache()
 
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: linkChain,
   cache,
 })
 
